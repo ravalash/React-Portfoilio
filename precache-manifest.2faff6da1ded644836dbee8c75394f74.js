bb127fe95e33d5f42fa45aedeedf9dd4f2603593
Ravalash <13k0873juhky@opayq.net>
2020-07-18 22:51:48 -0400

    Updates

---
+++ precache-manifest.2faff6da1ded644836dbee8c75394f74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "01a90f59268da8194627",
+    "url": "/React-Portfoilio/static/css/main.e913524c.chunk.css"
+  },
+  {
+    "revision": "01a90f59268da8194627",
+    "url": "/React-Portfoilio/static/js/main.390f3ed5.chunk.js"
+  },
+  {
+    "revision": "66636f56a27361d4c072",
+    "url": "/React-Portfoilio/static/js/runtime~main.bb3565e3.js"
+  },
+  {
+    "revision": "f5a0adc8d0bc3fbf7243",
+    "url": "/React-Portfoilio/static/js/2.5145a938.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/React-Portfoilio/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "4b133416b6202886b8e27cfc54a822bf",
+    "url": "/React-Portfoilio/index.html"
+  }
+];

6903bca4715a8d00ef46a4037a4194ce58c418f3
Ravalash <13k0873juhky@opayq.net>
2020-07-20 19:23:32 -0400

    Updates

---
+++ precache-manifest.8d42283cd7c24d033b12141cc57bc2d4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e08db5d39bc3804bdc3",
+    "url": "/React-Portfoilio/static/css/main.5be72520.chunk.css"
+  },
+  {
+    "revision": "5e08db5d39bc3804bdc3",
+    "url": "/React-Portfoilio/static/js/main.3399ecf6.chunk.js"
+  },
+  {
+    "revision": "66636f56a27361d4c072",
+    "url": "/React-Portfoilio/static/js/runtime~main.bb3565e3.js"
+  },
+  {
+    "revision": "c230a753da709797bbfc",
+    "url": "/React-Portfoilio/static/js/2.f9f5e4c5.chunk.js"
+  },
+  {
+    "revision": "812fede92e8f1bdb3d38aa06fddaf44e",
+    "url": "/React-Portfoilio/static/media/leather_1.812fede9.png"
+  },
+  {
+    "revision": "6bb6e193c21f23d57a26e46e85e98da6",
+    "url": "/React-Portfoilio/static/media/profile picture.6bb6e193.jpg"
+  },
+  {
+    "revision": "84c0e6594b8409518bd63416079ad4ea",
+    "url": "/React-Portfoilio/static/media/workday.84c0e659.jpg"
+  },
+  {
+    "revision": "3075e320611ca6a182e160ffaa928598",
+    "url": "/React-Portfoilio/static/media/missmarymagic.3075e320.jpg"
+  },
+  {
+    "revision": "e0cbf1258f05356408506b9d16ad96aa",
+    "url": "/React-Portfoilio/static/media/triviagame.e0cbf125.JPG"
+  },
+  {
+    "revision": "548fac85803e24e8c9e75b3fc3ec9bc4",
+    "url": "/React-Portfoilio/static/media/weatherdashboard.548fac85.JPG"
+  },
+  {
+    "revision": "fff8fb2260ed7f2f7c5d4872edfb4724",
+    "url": "/React-Portfoilio/static/media/movieme.fff8fb22.jpg"
+  },
+  {
+    "revision": "904eba2b21fae1aa3e411f107154b4a8",
+    "url": "/React-Portfoilio/static/media/passwordgenerator.904eba2b.JPG"
+  },
+  {
+    "revision": "2221ae3ad59d3206690b9494cf4ac2a6",
+    "url": "/React-Portfoilio/static/media/eatdaburger.2221ae3a.jpg"
+  },
+  {
+    "revision": "a2373e9c6f04f35bf8e4d18de328f7f0",
+    "url": "/React-Portfoilio/static/media/globeplotter.a2373e9c.jpg"
+  },
+  {
+    "revision": "5a128efc5828b8d097b7d852c6a8b1cc",
+    "url": "/React-Portfoilio/static/media/Adam Parsons Resume.5a128efc.pdf"
+  },
+  {
+    "revision": "ed011307a4ff1ebdc63fed1e3dfc56f7",
+    "url": "/React-Portfoilio/index.html"
+  }
+];
